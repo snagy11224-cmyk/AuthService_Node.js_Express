8b17ef5eda2e5db5ba1b91086204695d95119cd3
snagy11224-cmyk <snagy11224@gmail.com>
2026-01-20 00:51:21 +0200

    stopped exposing password in register endpoint

--- src/users/controllers/user.controller.js
+++ src/users/controllers/user.controller.js
@@ -5,7 +5,7 @@ exports.register = async (req, res) => {
   const { email, password } = req.body;             
     try {   
         const user = await userService.registerUser(email, password);
-        res.status(201).json({ message: "User registered successfully", user });
+        res.status(201).json({ message: "User registered successfully", email: user.email });
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
