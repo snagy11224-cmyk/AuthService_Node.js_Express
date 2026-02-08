9889f1df5040cc5804603a9c3be2cad7865e79b3
snagy11224-cmyk <snagy11224@gmail.com>
2026-02-09 00:54:05 +0200

    fixed connection and tried out

--- src/app/common/DB/prisma.js
+++ src/app/common/DB/prisma.js
@@ -1,3 +1,3 @@
-const {prismaClient} = require("@prisma/client"); //prisma client--> to interact with the database
-const prisma = new prismaClient(); //open connection to the database && called once in the app
+const {PrismaClient} = require('@prisma/client'); //prisma client--> to interact with the database
+const prisma = new PrismaClient(); //open connection to the database && called once in the app
 module.exports = prisma;
